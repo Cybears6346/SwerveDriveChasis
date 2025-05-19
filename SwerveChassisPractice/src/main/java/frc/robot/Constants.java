4dfc1b5f9979ad9119fea70079ff4e18ccac2376
T3rryYan9 <123417941+T3rryYan9@users.noreply.github.com>
2025-05-19 08:22:53 -0400

    Decluttered Repo; Removed example files and removed comments

--- SwerveChassisPractice/src/main/java/frc/robot/Constants.java
+++ SwerveChassisPractice/src/main/java/frc/robot/Constants.java
@@ -1,17 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
-
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 
 // Visual Representation of Swerve Chasis 
 
@@ -19,8 +6,6 @@ package frc.robot;
 //  Module 1, Module 2
 //  Module 3, Module 4
 //  Back
-
-
 
 public final class Constants {
   public static class OperatorConstants {
@@ -42,16 +27,11 @@ public final class Constants {
     public static final int driveMotor4ID = 7; 
     public static final int turnMotor4ID = 8;
 
-    //Mechanical Motor Constants
-
+    //Mechanical Constants
+    public static final int pigeonID = 0;
   
     //Gear Ratio/ Misc Constants
 
-    public static final int pigeonID = 0;
-
-
-    
-    
   }
 
 }
