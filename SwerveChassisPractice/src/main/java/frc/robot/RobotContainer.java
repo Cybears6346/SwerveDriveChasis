627b8479c141cce5530cf8730d559b7574185c54
T3rryYan9 <123417941+T3rryYan9@users.noreply.github.com>
2025-05-19 08:14:17 -0400

    Declared Pigeon2 Instance

--- SwerveChassisPractice/src/main/java/frc/robot/RobotContainer.java
+++ SwerveChassisPractice/src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,18 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+////////////////////////////////////////////  DELETE WHEN DONE
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+  ////////////////////////////////////////////  DELETE WHEN DONE
+
+//Motor Libraries
+
+//Pigeon Library
+import com.ctre.phoenix6.hardware.Pigeon2;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,8 +26,14 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+  //Subsystems
+  private final Pigeon2 s_pigeon2 = new Pigeon2(0,"rio");
+
+
+////////////////////////////////////////////  DELETE WHEN DONE
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+////////////////////////////////////////////  DELETE WHEN DONE
+
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
